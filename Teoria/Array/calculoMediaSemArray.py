6b9bad66737b28c99cc29130d3a5c5a4c0986487
Taynara Pinheiro <taynara.pinheirorj@gmail.com>
2023-11-12 13:27:38 -0300

    Média bimestral sem array
    exercício 4 usado como exemplos.

---
+++ Teoria/Array/calculoMediaSemArray.py
@@ -0,0 +1,12 @@
+
+# Declaração das variaveis atribuição
+nota1 = float(input('Digite nota 1 '))
+nota2 = float(input('Digite nota 2 '))
+nota3 = float(input('Digite nota 3 '))
+nota4 = float(input('Digite nota 4 '))
+
+soma = nota1 + nota2 + nota3 + nota4
+
+media = soma / 4
+
+print(f"Média: {media:.2f}")
