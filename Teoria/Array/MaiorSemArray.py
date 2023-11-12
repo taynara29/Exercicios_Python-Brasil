dcd7987350aea47a7fa94c03b7cd9c9ee19ca486
Taynara Pinheiro <taynara.pinheirorj@gmail.com>
2023-11-12 13:35:46 -0300

    Maior número digitado
    sem array

---
+++ Teoria/Array/MaiorSemArray.py
@@ -0,0 +1,16 @@
+
+nota1 = float(input('Digite nota 1 '))
+
+maior = nota1
+
+nota2 = float(input('Digite nota 2 '))
+
+if(maior < nota2):
+    maior = nota2
+
+nota3 = float(input('Digite nota 3 '))
+
+if(maior < nota3):
+    maior = nota3
+
+print(f"o maior numero e {maior}")
